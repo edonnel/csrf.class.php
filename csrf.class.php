0f75e0504ce7507b54ff4a076d1a7c84a7e5a375
Evan Donnel <35748349+edonnel@users.noreply.github.com>
2021-08-29 21:38:45 -0400

    Update csrf.class.php

--- csrf.class.php
+++ csrf.class.php
@@ -6,24 +6,28 @@
 				$_SESSION['csrf_token'] = bin2hex(random_bytes(32));
 		}
 
-		public static function validate() {
+		public static function validate($csrf_token = false) {
 			$return = array(
 				'success'   => false,
 				'msg'       => 'Unknown CSRF Error',
 			);
 
-			init_csrf();
+			self::init_csrf();
 
-			$headers = apache_request_headers();
+			if (!$csrf_token) {
 
-			if (isset($headers['Csrftoken']))
-				$csrf_token = $headers['Csrftoken'];
-			elseif (isset($headers['CsrfToken']))
-				$csrf_token = $headers['CsrfToken'];
-			elseif (isset($headers['csrftoken']))
-				$csrf_token = $headers['csrftoken'];
-			else
-				$csrf_token = false;
+				$headers = apache_request_headers();
+
+				if (isset($headers['Csrftoken']))
+					$csrf_token = $headers['Csrftoken'];
+				elseif (isset($headers['CsrfToken']))
+					$csrf_token = $headers['CsrfToken'];
+				elseif (isset($headers['csrftoken']))
+					$csrf_token = $headers['csrftoken'];
+				else
+					$csrf_token = false;
+
+			}
 
 			if ($csrf_token) {
 				if (!hash_equals($csrf_token, $_SESSION['csrf_token'])) {
